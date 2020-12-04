b65e8b9cba439c44bb5685b3366a64073f5a45ff
osanir <onursanir@gmail.com>
2020-12-04 12:34:50 +0300

    Removed test commits & Beutified main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
 #include "Game.h"
+
 class Player : public Entity{
 public:
 	Player() : Entity(sf::Vector2f(50, 50), sf::Vector2f(50, 50), sf::Color::Yellow) {
 	}
-
-private:
-
 };
+
 int main(){
 	Game game;
 	Entity* entity = new Player;
